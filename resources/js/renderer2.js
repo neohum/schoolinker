911fd8b902a935844cdd4387f57a17c6768af7fc
unknown <neohum@nate.com>
2024-05-05 22:08:49 +0900

    dev/20240505_001:complete

---
+++ resources/js/renderer2.js
@@ -0,0 +1,23 @@
+const { ipcRenderer } = require('electron')
+
+var linker_test = document.getElementById("linker_test")
+var linker_make = document.getElementById("linker_make")
+let fs = require('fs');
+fs.readFile('school_uid.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+  linker_make.setAttribute("href", `https://neohum775.mycafe24.com/www/linker_make/${data}`)
+  linker_test.setAttribute("href", `https://neohum775.mycafe24.com/www/linker_test/${data}`)
+})
+
+function closeApp(e) {
+  e.preventDefault()
+  
+  console.log("close app");
+  ipcRenderer.send('close')
+}
+
+document.getElementById("closeBtn").addEventListener("click", closeApp);
