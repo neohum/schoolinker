0f92304d08ffef3df38ad3444a53ee1667209cae
unknown <neohum@nate.com>
2024-07-06 08:34:45 +0900

    dev/20240706_001:add computer info

--- resources/js/renderer2.js
+++ resources/js/renderer2.js
@@ -1,22 +1,46 @@
 const { ipcRenderer, shell } = require('electron')
+const macaddress = require('macaddress');
+const os = require('os');
+const fs = require('fs');
 
-var linker_test = document.getElementById("linker_test")
-var linker_make = document.getElementById("linker_make")
-let fs = require('fs');
+let ipv4_1 = '';
+let memory = 0;
+let hostname = '';
+
 fs.readFile('school_uid.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err)
     return
   }
-  console.log(data)
-  if (linker_test == null){
-    console.log("linker_make is null")
-  } else {
-    shell.openExternal(`https://neohum775.mycafe24.com/www/linker_test/${data}`)
-  }
-  linker_make.setAttribute("href", `https://neohum775.mycafe24.com/www/linker_make/${data}`)
-  linker_test.setAttribute("href", `https://neohum775.mycafe24.com/www/linker_test/${data}`)
+  
+  
 })
+
+function openLinkerTest() {
+  fs.readFile('school_uid.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    shell.openExternal(`https://neohum775.mycafe24.com/www/linker_test/${data}`)
+  })
+
+}
+
+function openLinkerMake() {
+  fs.readFile('school_uid.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    shell.openExternal(`https://neohum775.mycafe24.com/www/linker_make/${data}`)
+  })
+
+}
+
+function openLink() {
+  shell.openExternal('https://neohum775.mycafe24.com/')
+}
 
 function closeApp(e) {
   e.preventDefault()
@@ -24,5 +48,26 @@ function closeApp(e) {
   console.log("close app");
   ipcRenderer.send('close')
 }
+
+function computerInfo() {
+  
+  macaddress.all().then(function (all) {
+    ipv4 = all.이더넷.ipv4;
+    console.log("IP Address:", ipv4);
+    ipv4_1 = ipv4;
+  })
+  memory = os.totalmem() / 1024 / 1024/ 1024;
+  hostname = os.hostname();
+ setTimeout(() => {
+    document.getElementById("memory").innerHTML = "메모리: " + memory.toFixed(2) + "GB";
+    document.getElementById("hostname").innerHTML = "컴퓨터 이름: " + hostname;
+    document.getElementById("ipv4").innerHTML = "아이피: " + ipv4_1;
+  }, 1000);
+  document.getElementById("memory").innerHTML = "메모리: " + memory.toFixed(2) + "GB";
+  document.getElementById("hostname").innerHTML = "컴퓨터 이름: " + hostname;
+  document.getElementById("ipv4").innerHTML = "아이피: " + ipv4_1;
+}
+
+computerInfo();
 
 document.getElementById("closeBtn").addEventListener("click", closeApp);
