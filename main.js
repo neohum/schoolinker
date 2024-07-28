60e2a68958a09d6920980ca2bd0f0507b6787b00
unknown <neohum@nate.com>
2024-07-28 10:47:29 +0900

    dev/20240728_001:add auto update and version text

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
+const { app, BrowserWindow, ipcMain,  dialog } = require('electron/main')
+const log = require('electron-log')
 let fs = require('fs');
+const { autoUpdater } = require('electron-updater');
+
+
+const server = 'https://github.com/neohum/schoolinker/releases/tag/'
+const url = `${server}/${app.getVersion()}`
+let updateInterval = null;
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 6000)
+
+autoUpdater.on('update-avilable', (_event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Ok'],
+    title: 'Update Available',
+    message: process.platform === 'win64' ? releaseNotes : releaseName,
+    detail:
+      'A new version download started. The app will be restarted to install the update.'
+  }
+
+  dialog.showMessageBox(dialogOpts);
+  
+  updateInterval = null;
+})
+
+
+autoUpdater.on('update-downloaded', (_event, releaseNotes, releaseName) => {
+   const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title:'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+   };
+   dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+   });
+});
+autoUpdater.on('error', (message) => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
+
+
 const createWindow = () => {
   if (fs.existsSync('school_uid.txt')) {
     const win = new BrowserWindow({
@@ -56,12 +102,45 @@ ipcMain.on('saveData', (event, data) => {
   
 })
 
+
+
+autoUpdater.on('checking-for-update', () => {
+  log.info('Checking for update...')
+})
+
+autoUpdater.on('update-available', (info) => {
+  log.info('Update available.')
+})
+autoUpdater.on('update-not-available', (info) => {
+  log.info('Update not available.')
+})
+autoUpdater.on('error', (err) => {
+  log.info('Error in auto-updater. ' + err)
+})
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+  log.info(log_message)
+})
+
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('Update downloaded')
+  autoUpdater.quitAndInstall()
+})
+
 app.whenReady().then(() => {
   createWindow()
+
+  autoUpdater.checkoForUpdates()
+
+  updateInterval = setInterval(() => autoUpdater.checkForUpdates(), 600000);
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
+      autoUpdater.checkoForUpdates()
 
     }
   })
--- resources/js/renderer2.js
+++ resources/js/renderer2.js
@@ -3,7 +3,7 @@ const macaddress = require('macaddress');
 const os = require('os');
 const fs = require('fs');
 
-let ipv4_1 = '';
+let ipv4 = '';
 let memory = 0;
 let hostname = '';
 
@@ -52,22 +52,61 @@ function closeApp(e) {
 function computerInfo() {
   
   macaddress.all().then(function (all) {
-    ipv4 = all.이더넷.ipv4;
-    console.log("IP Address:", ipv4);
-    ipv4_1 = ipv4;
+    //ipv4 = all.이더넷.ipv4;
+    //console.log("IP Address:", all);
+    ipv4 = JSON.stringify(all, null, 2);
+    if ("ipv4" in all.이더넷) {
+      ipv4 = all.이더넷.ipv4;
+    } else if ("ipv4" in all.이더넷1) {
+      ipv4 = all.이더넷1.ipv4;
+    } else if ("ipv4" in all.이더넷2) {
+      ipv4 = all.이더넷2.ipv4;
+    } else if ("ipv4" in all.이더넷3) {
+      ipv4 = all.이더넷3.ipv4;
+    } else if ("ipv4" in all.이더넷4) {
+      ipv4 = all.이더넷4.ipv4;
+    } else if ("ipv4" in all.이더넷5) {
+      ipv4 = all.이더넷5.ipv4;
+    } else if ("ipv4" in all.이더넷6) {
+      ipv4 = all.이더넷6.ipv4;
+    } else if ("ipv4" in all.이더넷7) {
+      ipv4 = all.이더넷7.ipv4;
+    } else if ("ipv4" in all.이더넷8) {
+      ipv4 = all.이더넷8.ipv4;
+    }
+    else {
+      ipv4 = "IP 주소를 찾을 수 없습니다.";
+    }
+
+    autoUpdater.on('checking-for-update', () => {
+  log.info('업데이트 확인 중...');
+});
+
+    
   })
+  
   memory = os.totalmem() / 1024 / 1024/ 1024;
   hostname = os.hostname();
+  
  setTimeout(() => {
-    document.getElementById("memory").innerHTML = "메모리: " + memory.toFixed(2) + "GB";
+    document.getElementById("memory").innerHTML = "메모리: " + Math.round(memory.toFixed(2)) + "GB";
     document.getElementById("hostname").innerHTML = "컴퓨터 이름: " + hostname;
-    document.getElementById("ipv4").innerHTML = "아이피: " + ipv4_1;
+    document.getElementById("ipv4").innerHTML = "아이피: " + ipv4;
   }, 1000);
-  document.getElementById("memory").innerHTML = "메모리: " + memory.toFixed(2) + "GB";
+  document.getElementById("memory").innerHTML = "메모리: " + Math.round(memory.toFixed(2)) + "GB";
   document.getElementById("hostname").innerHTML = "컴퓨터 이름: " + hostname;
-  document.getElementById("ipv4").innerHTML = "아이피: " + ipv4_1;
+  document.getElementById("ipv4").innerHTML = "아이피: " + ipv4;
 }
 
-computerInfo();
+function computerInfoOff() {
+  document.getElementById("memory").innerHTML = "";
+  document.getElementById("hostname").innerHTML = "";
+  document.getElementById("ipv4").innerHTML = "";
+
+}
+
+computerInfo()
+
+
 
 document.getElementById("closeBtn").addEventListener("click", closeApp);
